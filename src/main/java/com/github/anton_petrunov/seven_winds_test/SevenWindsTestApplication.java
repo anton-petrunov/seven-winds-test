04b0bfd0faaccce44d8bdb60c74a7c0c0c3f1a48
Anton Petrunov <a.n.tone@mail.ru>
2022-12-15 12:48:40 +0300

    Initial Commit

---
+++ src/main/java/com/github/anton_petrunov/seven_winds_test/SevenWindsTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.anton_petrunov.seven_winds_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenWindsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenWindsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/anton_petrunov/seven_winds_test/SevenWindsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.anton_petrunov.seven_winds_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenWindsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
