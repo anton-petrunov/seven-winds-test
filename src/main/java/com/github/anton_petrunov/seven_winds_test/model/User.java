520fa4bc3e88ea6a9982c756b6edf5df3fc0781f
Anton Petrunov <a.n.tone@mail.ru>
2022-12-15 22:12:40 +0300

    Update User

--- src/main/java/com/github/anton_petrunov/seven_winds_test/model/User.java
+++ src/main/java/com/github/anton_petrunov/seven_winds_test/model/User.java
@@ -1,13 +1,14 @@
 package com.github.anton_petrunov.seven_winds_test.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @AllArgsConstructor
