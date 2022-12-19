10708d00569c155666baf268b5613b13c550695b
Anton Petrunov <a.n.tone@mail.ru>
2022-12-19 11:37:25 +0300

    Change project to Flyway

---
+++ src/main/resources/db/migration/V1_1__populate_database.sql
@@ -0,0 +1,6 @@
+DELETE
+FROM users;
+
+INSERT INTO users (email, surname, name, patronymic, phone)
+VALUES ('petrunov.ru@gmail.com', 'petrunov', 'Anton', 'Nikolaich', '+79312211019'),
+       ('petrunov@kirill.ru', 'Petrunov', 'Kirill', 'Antonovich', '+79312211019');
