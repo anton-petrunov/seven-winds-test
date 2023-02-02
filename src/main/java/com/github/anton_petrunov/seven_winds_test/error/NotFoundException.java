5c0d521f3598d8b889983fba2c1ceec42dce61c3
Anton Petrunov <a.n.tone@mail.ru>
2023-02-02 16:42:33 +0300

    Delete unused exceptions and ValidationUtil

--- src/main/java/com/github/anton_petrunov/seven_winds_test/error/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.anton_petrunov.seven_winds_test.error;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
