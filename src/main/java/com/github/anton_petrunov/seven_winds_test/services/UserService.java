e7964b34879c9b35265cd0582f21a9a59856844c
Anton Petrunov <a.n.tone@mail.ru>
2023-02-02 16:40:48 +0300

    Update UserService and UserRestController

--- src/main/java/com/github/anton_petrunov/seven_winds_test/services/UserService.java
+++ src/main/java/com/github/anton_petrunov/seven_winds_test/services/UserService.java
@@ -1,5 +1,7 @@
 package com.github.anton_petrunov.seven_winds_test.services;
 
+import com.github.anton_petrunov.seven_winds_test.error.EntityNotFoundException;
+import com.github.anton_petrunov.seven_winds_test.error.EntityNotNewException;
 import com.github.anton_petrunov.seven_winds_test.model.User;
 import com.github.anton_petrunov.seven_winds_test.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -17,10 +19,13 @@ public class UserService {
     }
 
     public User get(Integer id) {
-        return userRepository.findById(id).orElseThrow();
+        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(User.class, "id", id.toString()));
     }
 
     public User create(User user) {
+        if (!user.isNew()) {
+            throw new EntityNotNewException(user.getClass(), user.getId());
+        }
         return userRepository.save(user);
     }
 }
--- src/main/java/com/github/anton_petrunov/seven_winds_test/web/UserRestController.java
+++ src/main/java/com/github/anton_petrunov/seven_winds_test/web/UserRestController.java
@@ -1,5 +1,6 @@
 package com.github.anton_petrunov.seven_winds_test.web;
 
+import com.github.anton_petrunov.seven_winds_test.error.EntityNotFoundException;
 import com.github.anton_petrunov.seven_winds_test.model.User;
 import com.github.anton_petrunov.seven_winds_test.services.UserService;
 import com.github.anton_petrunov.seven_winds_test.to.UserTo;
@@ -7,18 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 
 import static com.github.anton_petrunov.seven_winds_test.util.UserUtil.createTo;
 import static com.github.anton_petrunov.seven_winds_test.util.UserUtil.createTos;
-import static com.github.anton_petrunov.seven_winds_test.util.ValidationUtil.checkNew;
-import static com.github.anton_petrunov.seven_winds_test.util.ValidationUtil.checkNotFoundWithId;
 
 //TODO: добавить пагинацию для метода getAll(), будем возвращать данные постранично
 @RestController
@@ -35,20 +31,15 @@ public class UserRestController {
     }
 
     @GetMapping(value = "/{id}")
-    public UserTo get(@PathVariable Integer id) {
+    public UserTo get(@PathVariable Integer id) throws EntityNotFoundException {
         log.info("get user {}", id);
-        return createTo(checkNotFoundWithId(userService.get(id), id));
+        return createTo(userService.get(id));
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<UserTo> create(@Valid @RequestBody User user) {
+    public UserTo create(@Valid @RequestBody User user) {
         log.info("create new {}", user);
-        checkNew(user);
-        UserTo userTo = createTo(userService.create(user));
-        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/users" + "/{id}")
-                .build().toUri();
-        return ResponseEntity.created(uriOfNewResource).body(userTo);
+        return createTo(userService.create(user));
     }
 }
